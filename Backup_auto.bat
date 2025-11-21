caf001cf675a426e406617d7e2d88aea9cef3b39
Xavo <losruizguevara@gmail.com>
2025-11-21 11:42:28 -0500

    Backup automatico 2025- 2-vi

--- Backup_auto.bat
+++ Backup_auto.bat
@@ -5,7 +5,7 @@ set FECHA=%date:~-4%-%date:~3,2%-%date:~0,2%
 set ARCHIVO=Respaldito_%FECHA%.backup
 
 echo Creando backup: %ARCHIVO%
-pg_dump --host localhost --port 5432 --username "postgres" --format custom --file "C:\Users\losru\OneDrive\Escritorio\Backups\%ARCHIVO%" "Chambta"
+pg_dump --host localhost --port 5432 --username "postgres" --format custom --file "C:\Users\losru\OneDrive\Escritorio\Backups\%ARCHIVO%" "Chamba"
 
 REM === 2. MOVERSE A LA CARPETA DEL REPO ===
 cd /d "C:\Users\losru\OneDrive\Escritorio\Backups"
