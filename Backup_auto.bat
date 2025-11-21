3e26bcfc3cc37f99e6597b02baf3e6bccc42e108
Xavo <losruizguevara@gmail.com>
2025-11-21 07:19:14 -0500

    Backup automatico 2025- 2-vi

---
+++ Backup_auto.bat
@@ -0,0 +1,20 @@
+@echo off
+REM === 1. CREAR BACKUP CON PG_DUMP ===
+
+set FECHA=%date:~-4%-%date:~3,2%-%date:~0,2%
+set ARCHIVO=Respaldito_%FECHA%.backup
+
+echo Creando backup: %ARCHIVO%
+pg_dump --host localhost --port 5432 --username "postgres" --format custom --file "C:\Users\losru\OneDrive\Escritorio\Backups\%ARCHIVO%" "Chambta"
+
+REM === 2. MOVERSE A LA CARPETA DEL REPO ===
+cd /d "C:\Users\losru\OneDrive\Escritorio\Backups"
+
+REM === 3. AGREGAR CAMBIOS A GIT ===
+git add .
+git commit -m "Backup automatico %FECHA%"
+git push origin main
+
+echo.
+echo TODO COMPLETADO EXITOSAMENTE
+pause
